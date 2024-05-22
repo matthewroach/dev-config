8ea9bdc8b157883443980e887de4befcf64e066c
Matthew Roach <m@roach.is>
2024-05-22 13:56:42 +0100

    Add eslint config

---
+++ eslint/index.js
@@ -0,0 +1,28 @@
+// index.js
+module.exports = {
+	extends: [
+		'next',
+		'next/core-web-vitals',
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:yaml/recommended',
+		'plugin:prettier/recommended',
+	],
+	plugins: ['@typescript-eslint', 'prettier', 'yaml'],
+	ignorePatterns: [
+		'.generated/**/*',
+		'**/*.d.ts',
+		'**/*.js',
+		'*.targets',
+		'**/temp/*',
+	],
+	rules: {
+		'@next/next/no-img-element': 'off', // Don't force next/image
+		'jsx-a11y/alt-text': ['warn', { elements: ['img'] }], // Don't force alt for <Image/> (sourced from Sitecore media)
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@next/next/no-html-link-for-pages': ['error', './src/hcamain/src/pages/'],
+		'react/no-children-prop': 'off',
+	},
+}
